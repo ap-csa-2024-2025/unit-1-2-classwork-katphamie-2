d4406a3163f346b135574eaf282f2c6ea5ed6a24
katphamie <katnphmie@gmail.com>
2024-09-05 10:12:20 -0700

    1.2 hw

--- problem2.java
+++ problem2.java
@@ -1,1 +1,12 @@
 // Create your class here; don't forget the main method
+public class problem2
+{
+    public static void main(String [] args)
+    {
+        String name1 = "Simba";
+        String name2 = "Lester";
+        String name3 = "Erika";
+        String name4 = "Miller";
+        System.out.print(name2 + " " + name4 + " " + name1 + " " + name3);
+    }
+}
--- problem3.java
+++ problem3.java
@@ -1,1 +1,10 @@
 // Create your class here; don't forget the main method
+public class problem3
+{
+    public static void main(String[] args)
+    {
+        String name = "Kathleen";
+        String state = "California";
+        System.out.print("My name is " + name + ". I live in " + state + ".");
+    }
+}
